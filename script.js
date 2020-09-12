b41eb220d6cefafbe2fb982993dae5629a5ad29c
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-09-12 22:15:09 +0200

    Update DOM and Add Pagination

--- script.js
+++ script.js
@@ -3,30 +3,58 @@ const search=document.getElementById('search');
 const result=document.getElementById('result');
 const more=document.getElementById('more');
 
-const aoiURL='https://api.lyrics.ovh';
+const apiURL='https://api.lyrics.ovh';
 
 //Search bt song or artist
 async function searchSongs(term){
-    
-
     const res=await fetch(`${apiURL}/suggest/${term}`);
+    const data =await res.json();
+    
+    showData(data);
+};
+
+//Show song and artist in DOM
+function showData(data){
+   
+    result.innerHTML=`
+        <ul class="songs">
+            ${data.data.map(song=>`<li>
+            <span><strong>${song.artist.name}</strong>- ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}"data-songtitle="${song.title}">Get Lyrics</button>
+            </li>`)
+            .join('')
+        }
+        </ul>
+    `;
+
+    if(data.prev || data.next){
+
+        more.innerHTML=`
+            ${data.prev ? `<button class="btn" onClick="getMoreSongs('${data.prev}')">Prev</button>` :''}
+            ${data.next ? `<button class="btn" onClick="getMoreSongs('${data.next}')">Next</button>` :''}
+        `
+    }else{
+        more.innerHTML='';
+    }
+}
+
+//Get prev and next songs
+async function getMoreSongs(url){
+    const res=await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
     const data =await res.json();
     
     showData(data);
 }
 
-//Show song and artist in DOM
-function showData(data){}
-
 //Event listeners
 form. addEventListener('submit', (e)=>{
     e.preventDefault();
 
-    const searchTerm=search.value0.trim();
+    const searchTerm=search.value.trim();
 
     if(!searchTerm){
         alert('Please type in a search term');
     }else{
         searchSongs(searchTerm)
     }
-})
+});
